e675645c64f5f10cc29c3a55c14db2c6e4938102
Woody Sims <sims.woodrow@gmail.com>
2022-12-02 20:42:57 -0600

    formatted code to be pep8 compliant!

--- explore.py
+++ explore.py
@@ -13,9 +13,10 @@ import wrangle as w
 
 warnings.filterwarnings('ignore')
 
-def p_to_md(p:float,alpha:float=.05,**kwargs)->md:
+
+def p_to_md(p: float, alpha: float = .05, **kwargs) -> md:
     '''
-    returns the result of a p test as a `IPython.display.Markdown`
+    returns the result of a p test as a `Markdown` object
     ## Parameters
     p: `float` of the p value from performed Hypothesis test
     alpha: `float` of alpha value for test, defaults to 0.05
@@ -26,12 +27,14 @@ def p_to_md(p:float,alpha:float=.05,**kwargs)->md:
     '''
     ret_str = ''
     p_flag = p < alpha
-    for k,v in kwargs.items():
+    for k, v in kwargs.items():
         ret_str += f'## {k} = {v}\n\n'
     ret_str += f'## Because $\\alpha$ {">" if p_flag else "<"} p,' + \
         f'we {"failed to " if ~(p_flag) else ""} reject $H_0$'
     return md(ret_str)
-def t_to_md(p:float,t:float,alpha:float=.05,**kwargs):
+
+
+def t_to_md(p: float, t: float, alpha: float = .05, **kwargs):
     '''takes a p-value, alpha, and any T-test arguments and
     creates a Markdown object with the information.
     ## Parameters
@@ -45,161 +48,199 @@ def t_to_md(p:float,t:float,alpha:float=.05,**kwargs):
     t_flag = t > 0
     p_flag = p < alpha
     ret_str += f'## t = {t} \n\n'
-    for k,v in kwargs.items():
+    for k, v in kwargs.items():
         ret_str += f'## {k} = {v}\n\n'
     ret_str += f' ## p = {p} \n\n'
-    ret_str +=\
-         f'## Because t {">" if t_flag else "<"} 0 and $\\alpha$ {">" if p_flag else "<"} p,' + \
-            f'we {"failed to " if ~(t_flag & p_flag) else ""} reject $H_0$'
+    ret_str += (f'## Because t {">" if t_flag else "<"} 0 '
+                f'and $\\alpha$ {">" if p_flag else "<"} p, '
+                f'we {"failed to " if ~(t_flag & p_flag) else ""} '
+                ' reject $H_0$')
     return md(ret_str)
 
-def anova_test(df:pd.DataFrame,col:str):
+
+def anova_test(df: pd.DataFrame, col: str):
+    # TODO Naomi: change to new stats test and fill in docstring
     '''
-    TODO 
     ## Parameters
-    
+
     ## Returns
-    
+
     '''
     group_list = [df[df[col] == x].log_error.to_numpy() for x in range(4)]
-    t,p = stats.kruskal(group_list[0],group_list[1],group_list[2],group_list[3])
-    #cluster_3 = df[df.cluster3== 3]
-    return e.t_to_md(t,p)
+    t, p = stats.kruskal(
+        group_list[0], group_list[1], group_list[2], group_list[3])
+    # cluster_3 = df[df.cluster3== 3]
+    return t_to_md(t, p)
 
-#################### CREATING CLUSTERS
-def create_clusters_Q3(X_train, k, cluster_vars):
-    '''This function uses pool_count, garage_car_count, and lot_sqft 
-    to create a cluster for question 3'''
-    
-    k=4
+
+def create_clusters_pool_garage_lot_sqft(x_train: pd.DataFrame, k: int):
+    # TODO Naomi check this works and approve docstring
+    # I took out cluster_vars from the parameters and renamed the function
+    '''
+    Marks K-Means on pool count, garage car count, and lot square feet
+    ## Parameters
+    x_train: `DataFrame` containing training data features
+    k: `int` indicating number of centroids
+    ## Returns
+    Fitted `KMeans` object
+    '''
+
+    k = 4
     cluster_vars = ['pool_count', 'garage_car_count', 'lot_sqft']
-    cluster_name = 'X3'
-    
+    cluster_name = 'x3'
+
     # create kmean object
-    kmeans = KMeans(n_clusters=k, random_state = 89)
+    kmeans = KMeans(n_clusters=k, random_state=89)
     # fit to train and assign cluster ids to observations
-    kmeans.fit(X_train[cluster_vars])
+    kmeans.fit(x_train[cluster_vars])
 
     return kmeans
 
-def generate_elbow(df:pd.DataFrame,k_min:int = 1,k_max:int = 30)->None:
+
+def generate_elbow(df: pd.DataFrame, k_min: int = 1, k_max: int = 30) -> None:
     '''
-    TODO Woody
+    Plots KMeans elbow of a given potential cluster as well as the
+    percent change for the graph
     ## Parameters
-    
+    df: `DataFrame` containing features to perform KMeans clustering on
+    k_min: `int` specifying minimum number of centroids
+    k_max: `int` specifying maximum number of centroids
     ## Returns
-    
+    None (plots graph to Jupyter notebook)
     '''
-    inertia = {i:KMeans(i,random_state=420).fit(df).inertia_ for i in range(k_min,k_max)}
-    fig,axs = plt.subplots(1,2,figsize=(12,5))
-    sns.lineplot(data=inertia,ax=axs[0])
+    inertia = {i: KMeans(i, random_state=420).fit(
+        df).inertia_ for i in range(k_min, k_max)}
+    fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+    sns.lineplot(data=inertia, ax=axs[0])
     axs[0].set_title('Inertia')
     axs[0].set_xlabel('No. of Clusters')
     axs[0].set_ylabel('Inertia')
-    pct_change = [((inertia[i]-inertia[i+1])/inertia[i])*100 for i in range(k_min,k_max-1)]
-    sns.lineplot(data=pct_change,ax=axs[1])
+    pct_change = [((inertia[i]-inertia[i+1])/inertia[i])
+                  * 100 for i in range(k_min, k_max-1)]
+    sns.lineplot(data=pct_change, ax=axs[1])
     axs[1].set_xlabel('No. of Clusters')
     axs[1].set_ylabel('% of Change')
     axs[1].set_title('% Change')
     fig.tight_layout()
     plt.show()
-def viz_for_Q3(train_df:pd.DataFrame)->None:
+
+
+def viz_for_Q3(train_df: pd.DataFrame) -> None:
+    # TODO Naomi double check the docstring I wrote for this
     '''
-    TODO Naomi
+    Generates visualizations of both scaled and unscaled clusters
+    of garage car count, pool count, and lot square feet
     ## Parameters
     train_df: `DataFrame` containing training data.
     ## Returns
     None
     '''
-    #unscaled data
-    X3 = train_df[['garage_car_count', 'pool_count', 'lot_sqft']]
-    kmeans = KMeans(n_clusters = 4).fit(X3)
-    train_df['cluster3'] = kmeans.predict(X3)
-    
-    #scaled data
-    train_scaled3 = w.scale(train_df, ['garage_car_count', 'pool_count', 'lot_sqft'])
-    X3_scaled = train_scaled3[['scaled_garage_car_count', 'scaled_pool_count', 'scaled_lot_sqft']]
-    kmeans = KMeans(n_clusters = 4).fit(X3_scaled)
-    train_scaled3['cluster3_scaled'] = kmeans.predict(X3_scaled)
+    # unscaled data
+    x3_unscaled = train_df[['garage_car_count', 'pool_count', 'lot_sqft']]
+    kmeans = KMeans(n_clusters=4).fit(x3_unscaled)
+    train_df['cluster3'] = kmeans.predict(x3_unscaled)
+
+    # scaled data
+    train_scaled3 = w.scale(
+        train_df, ['garage_car_count', 'pool_count', 'lot_sqft'])
+    x3_scaled = train_scaled3[[
+        'scaled_garage_car_count', 'scaled_pool_count', 'scaled_lot_sqft']]
+    kmeans = KMeans(n_clusters=4).fit(x3_scaled)
+    train_scaled3['cluster3_scaled'] = kmeans.predict(x3_scaled)
     train_scaled3['log_error'] = train_df['log_error']
-    
-    #viz 
-    fig, axes= plt.subplots(1,2, figsize =(15, 10), sharey = True)
+
+    # viz
+    fig, axes = plt.subplots(1, 2, figsize=(15, 10), sharey=True)
     fig.subtitle = ('Unscaled vs. Scaled')
 
-    #Unscaled
-    sns.stripplot(ax=axes[0], data = train_df, x = 'cluster3', y = 'log_error')
+    # Unscaled
+    sns.stripplot(ax=axes[0], data=train_df, x='cluster3', y='log_error')
     axes[0].set_title('Unscaled')
 
-    #Scaled
-    sns.stripplot(ax = axes[1],data = train_scaled3, x = 'cluster3_scaled', y = 'log_error')
+    # Scaled
+    sns.stripplot(ax=axes[1], data=train_scaled3,
+                  x='cluster3_scaled', y='log_error')
     axes[1].set_title('Scaled')
-    
+
     plt.show()
 
-def cluster_kde(df:pd.DataFrame,cluster:str,target:str)->sns.FacetGrid:
-    facet = sns.FacetGrid(data=df,col_wrap=4,col=cluster)
-    facet = facet.map_dataframe(sns.boxplot,x=target)
-    return facet
 
-def cluster_fun(df:pd.DataFrame)->pd.DataFrame:
-    all_features = df[['bath_count','bed_count',
-    'calc_sqft','latitude','longitude','tax_value','fireplace_count']].columns
-    cluster_test =df.copy()
+def cluster_fun(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Creates `DataFrame` of a given list of features pairwise as well as plots
+    visualizations
+    ## Parameters
+    df: `DataFrame` containing features to cluster
+    ## Returns
+    `DataFrame` of clusters for data
+    '''
+    all_features = df[['bath_count', 'bed_count',
+                       'calc_sqft', 'latitude',
+                       'longitude', 'tax_value', 'fireplace_count']].columns
+    cluster_test = df.copy()
     for feature in all_features:
         for feature2 in all_features:
             if feature != feature2:
                 features_cluster = cluster_test[[feature, feature2]]
 
-                #Fit a new model to my scaled data
+                # Fit a new model to my scaled data
                 kmeans_scale = KMeans(n_clusters=4)
 
                 kmeans_scale.fit(features_cluster)
-                cluster_name = feature +'_'+ feature2 + '_cluster'
-                cluster_test[cluster_name] = kmeans_scale.predict(features_cluster)
+                cluster_name = feature + '_' + feature2 + '_cluster'
+                cluster_test[cluster_name] = kmeans_scale.predict(
+                    features_cluster)
                 plt.title(f'{feature} and {feature2} cluster')
                 sns.scatterplot(y='log_error', x=cluster_name,
-                        palette='colorblind', data=cluster_test)
+                                palette='colorblind', data=cluster_test)
                 plt.show()
     return cluster_test
 
 
-def tax_sqft_plot(df:pd.DataFrame)->None:
+def tax_sqft_plot(df: pd.DataFrame) -> None:
     '''
-    TODO Woody
+    Helper function which plots the tax value vs. the calculated square
+    footage of the zillow data set
     ## Parameters
-    
+    df: `DataFrame` containing training data
     ## Returns
-    
+    None (plots values to Jupyter notebook)
     '''
     sns.set_palette('magma')
-    fig,axs = plt.subplots(1,2)
+    fig, axs = plt.subplots(1, 2)
     df.log_error = df.log_error.astype('float')
-    sns.scatterplot(data=df,x='calc_sqft',y='tax_value',hue='log_error',ax=axs[0],palette='magma').\
-        set(yscale='log')
-    calc = df[np.abs(df.log_error) >=1]
-    sns.scatterplot(data=calc, x='calc_sqft',y='tax_value',hue='log_error',ax=axs[1],palette='magma').\
-        set(yscale='log')
+    sns.scatterplot(data=df, x='calc_sqft', y='tax_value',
+                    hue='log_error', ax=axs[0],
+                    palette='magma').set(yscale='log')
+    calc = df[np.abs(df.log_error) >= 1]
+    sns.scatterplot(data=calc, x='calc_sqft', y='tax_value',
+                    hue='log_error', ax=axs[1],
+                    palette='magma').set(yscale='log')
     fig.suptitle('Tax Value vs. Calculated Sqft.')
     axs[0].set_title('All Data')
     axs[1].set_title('Abs. Val of Log Error >= 1')
     plt.show()
-def tax_sqft_cluster_plot(train:pd.DataFrame)->None:
+
+
+def tax_sqft_cluster_plot(train: pd.DataFrame) -> None:
     '''
-    TODO Woody
+    Generates Histogram visualizations of tax value/calculated
+    square footage cluster.
     ## Parameters
-    
+    train: `DataFrame` containing training data as well as the
+    `tax_sqft_cluster` group for each value
     ## Returns
-    
+    None (plots visualizations to Jupyter notebook)
     '''
-    tax_sqft = train[['calc_sqft','tax_value']]
+    tax_sqft = train[['calc_sqft', 'tax_value']]
     tax_sqft['calc_sqft'] = train['calc_sqft']
     tax_sqft['tax_value'] = train['tax_value']
-    kmeans = KMeans(5,random_state=420)
+    kmeans = KMeans(5, random_state=420)
     kmeans.fit(tax_sqft)
     tax_sqft['tax_sqft_cluster'] = kmeans.predict(tax_sqft)
     tax_sqft['log_error'] = train.log_error
     sns.set_palette('magma')
-    g = sns.FacetGrid(data=tax_sqft,col='tax_sqft_cluster',col_wrap=2,sharey=True,palette='mako').set(yscale='log')
-    g.map_dataframe(sns.histplot,x='log_error')
+    g = sns.FacetGrid(data=tax_sqft, col='tax_sqft_cluster',
+                      col_wrap=2, sharey=True).set(yscale='log')
+    g.map_dataframe(sns.histplot, x='log_error')
     plt.show()
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
-'''model contains helper functions to assist in Modeling portion of final_report.ipynb'''
-from typing import Union,Tuple,Dict
+'''model contains helper functions to assist in Modeling
+portion of final_report.ipynb'''
+from typing import Union, Tuple, Dict
 
 import numpy as np
 import pandas as pd
@@ -11,7 +12,7 @@ import evaluate as ev
 from custom_dtypes import LinearRegressionType, ModelDataType
 
 
-def select_baseline(ytrain:pd.Series)->md:
+def select_baseline(ytrain: pd.Series) -> md:
     '''tests mean and median of training data as a baseline metric.
     ## Parameters
     ytrain: `pandas.Series` containing the target variable
@@ -20,14 +21,16 @@ def select_baseline(ytrain:pd.Series)->md:
     '''
     med_base = ytrain.median()
     mean_base = ytrain.mean()
-    mean_eval = ev.regression_errors(ytrain,mean_base,'Mean Baseline')
-    med_eval = ev.regression_errors(ytrain,med_base,'Median Baseline')
-    ret_md = pd.concat([mean_eval,med_eval]).to_markdown()
+    mean_eval = ev.regression_errors(ytrain, mean_base, 'Mean Baseline')
+    med_eval = ev.regression_errors(ytrain, med_base, 'Median Baseline')
+    ret_md = pd.concat([mean_eval, med_eval]).to_markdown()
     ret_md += '\n### Because mean outperformed median on all metrics, \
         we will use mean as our baseline'
     return md(ret_md)
-def linear_regression(x:pd.DataFrame,y:pd.DataFrame,\
-    linreg:Union[LinearRegression,None]=None)->None:
+
+
+def linear_regression(x: pd.DataFrame, y: pd.DataFrame,
+                      linreg: Union[LinearRegression, None] = None) -> None:
     '''runs linear regression on x and y
     ## Parameters
     x: DataFrame of features
@@ -43,18 +46,22 @@ def linear_regression(x:pd.DataFrame,y:pd.DataFrame,\
     '''
     if linreg is None:
         linreg = LinearRegression(normalize=True)
-        linreg.fit(x,y)
+        linreg.fit(x, y)
     ypred = linreg.predict(x)
-    return ypred,linreg
-def lasso_lars(x:pd.DataFrame,y:pd.DataFrame,llars:Union[None,LassoLars] = None)\
-    ->Tuple[np.array,LassoLars]:
+    return ypred, linreg
+
+
+def lasso_lars(x: pd.DataFrame, y: pd.DataFrame,
+               llars: Union[None, LassoLars] = None)\
+        -> Tuple[np.array, LassoLars]:
     '''runs LASSO+LARS on x and y
     ## Parameters
     x: Dataframe of features
 
     y: DataFrame of target
 
-    llars: Optional LASSO + LARS object, used if model has already been trained, default: None
+    llars: Optional LASSO + LARS object, used if model has
+    already been trained, default: None
     ## Returns
     ypred: numpy.array of predictions
 
@@ -62,59 +69,75 @@ def lasso_lars(x:pd.DataFrame,y:pd.DataFrame,llars:Union[None,LassoLars] = None)
     '''
     if llars is None:
         llars = LassoLars(alpha=3.0)
-        llars.fit(x,y)
+        llars.fit(x, y)
     ypred = llars.predict(x)
-    return ypred,llars
-def lgm(x:pd.DataFrame,y:pd.DataFrame, tweedie:Union[TweedieRegressor,None] = None)\
-    ->Tuple[np.array,TweedieRegressor]:
+    return ypred, llars
+
+
+def lgm(x: pd.DataFrame, y: pd.DataFrame,
+        tweedie: Union[TweedieRegressor, None] = None)\
+        -> Tuple[np.array, TweedieRegressor]:
     '''runs Generalized Linear Model (GLM) on x and y
     ## Parameters
     x: `DataFrame` of features
 
     y: 'DataFrame' of target
 
-    tweedie: `TweedieRegressor` object, used if model has already been trained, default: None
+    tweedie: `TweedieRegressor` object, used if model has already been trained,
+    default: None
     ## Returns
     ypred: numpy.array of predictions
 
     tweedie: `TweedieRegressor` model trained on data.
     '''
     if tweedie is None:
-        tweedie = TweedieRegressor(power=0,alpha=3.0)
-        tweedie.fit(x,y)
+        tweedie = TweedieRegressor(power=0, alpha=3.0)
+        tweedie.fit(x, y)
     ypred = tweedie.predict(x)
     return ypred, tweedie
-def rmse_eval(ytrue:Dict[str,np.array],**kwargs)->pd.DataFrame:
+
+
+def rmse_eval(ytrue: Dict[str, np.array],
+              **kwargs) -> pd.DataFrame:
     '''
     performs Root Mean Squared evaluation on parameters
     ## Parameters
-    ytrue: a dictionary of `numpy.array` containing the true Y values on which to evaluate
+    ytrue: a dictionary of `numpy.array` containing the true Y values
+    on which to evaluate
     kwargs: named dictionary of `numpy.array` objects
         with predicted y values where for each key:value
-    pain in ytrue there is a corresponding key:value pair in `kwargs[REGRESSION FUNCTION NAME]`
+    pain in ytrue there is a corresponding key:value pair in
+    `kwargs[REGRESSION FUNCTION NAME]`
     ## Returns
-    a `pandas.DataFrame` of Root Mean Squared Evaluation for each dataset in kwargs.
+    a `pandas.DataFrame` of Root Mean Squared Evaluation
+    for each dataset in kwargs.
     '''
     ret_df = pd.DataFrame()
-    for key,value in kwargs.items():
-        for k_key,v_value in value.items():
-            ret_df.loc[key,k_key] = np.round(np.sqrt(mean_squared_error(ytrue[k_key],v_value)),2)
+    for key, value in kwargs.items():
+        for k_key, v_value in value.items():
+            ret_df.loc[key, k_key] = np.round(
+                np.sqrt(mean_squared_error(ytrue[k_key], v_value)), 2)
     return ret_df
-def evaluate_models(xtrain:ModelDataType,ytrain:ModelDataType,\
-    xvalid:ModelDataType,yvalid:ModelDataType)->pd.DataFrame:
+
+
+def evaluate_models(xtrain: ModelDataType, ytrain: ModelDataType,
+                    xvalid: ModelDataType,
+                    yvalid: ModelDataType) -> pd.DataFrame:
     '''
-    Performs LASSO+LARS, Linear Regression, and Generalized Linear Model regression
-        on train and validate data sets provided
+    Performs LASSO+LARS, Linear Regression, and
+    Generalized Linear Model regression
+    on train and validate data sets provided
     ## Parameters
     xtrain: `pandas.DataFrame` containing x values from training dataset
         on which to perform regression fit and prediction
-    ytrain: `pandas.DataFrame` containing y values on which to perform regression prediction
+    ytrain: `pandas.DataFrame` containing y values
+    on which to perform regression prediction
     xvalid: `pandas.DataFrame` containing x values from validate data set
         on which to perform regression predictions
     yvalid: `pandas.DataFrame` containing y values from
         validate data set on which to perform regression predictions
     ## Returns
-    `IPython.display.Markdown` object containing table of Reverse Mean Squared Error
+    `Markdown` object containing table ofReverse Mean Squared Error
         evaluation of train and validate.
     data sets with each of the models.
 
@@ -123,24 +146,29 @@ def evaluate_models(xtrain:ModelDataType,ytrain:ModelDataType,\
     linreg_pred = dict()
     llars_pred = dict()
     glm_pred = dict()
-    linreg_pred['train'], linreg = linear_regression(xtrain,ytrain)
-    linreg_pred['validate'], _ = linear_regression(xvalid,yvalid,linreg)
-    llars_pred['train'],llars = lasso_lars(xtrain,ytrain)
-    llars_pred['validate'],_ = lasso_lars(xvalid,yvalid,llars)
-    glm_pred['train'], tweedie = lgm(xtrain,ytrain)
-    glm_pred['validate'],_ = lgm(xvalid,yvalid,tweedie)
-    ytrue = {'train':ytrain,'validate':yvalid}
+    linreg_pred['train'], linreg = linear_regression(xtrain, ytrain)
+    linreg_pred['validate'], _ = linear_regression(xvalid, yvalid, linreg)
+    llars_pred['train'], llars = lasso_lars(xtrain, ytrain)
+    llars_pred['validate'], _ = lasso_lars(xvalid, yvalid, llars)
+    glm_pred['train'], tweedie = lgm(xtrain, ytrain)
+    glm_pred['validate'], _ = lgm(xvalid, yvalid, tweedie)
+    ytrue = {'train': ytrain, 'validate': yvalid}
     baseline_pred = dict()
-    baseline_pred['train'] = np.full_like(\
-        np.arange(xtrain.shape[0],dtype=int),ytrain.tax_value.mean())
-    baseline_pred['validate'] = np.full_like(\
-        np.arange(xvalid.shape[0],dtype=int),ytrain.tax_value.mean())
-    evaluation_matrix = rmse_eval(ytrue,baseline=baseline_pred,linear_regression=linreg_pred,\
-        lasso_lars=llars_pred,glm=glm_pred)
-    evaluation_matrix.index= ['Baseline','Linear Regression','LASSO LARS','General Linear Model']
-    evaluation_matrix.columns = ['Train RMSE','Validate RMSE']
-    return md('| Methodology' + evaluation_matrix.to_markdown()[1:]),llars
-def run_test(model:LinearRegressionType,xtest:ModelDataType,ytest:ModelDataType)->md:
+    baseline_pred['train'] = np.full_like(
+        np.arange(xtrain.shape[0], dtype=int), ytrain.tax_value.mean())
+    baseline_pred['validate'] = np.full_like(
+        np.arange(xvalid.shape[0], dtype=int), ytrain.tax_value.mean())
+    evaluation_matrix = rmse_eval(ytrue, baseline=baseline_pred,
+                                  linear_regression=linreg_pred,
+                                  lasso_lars=llars_pred, glm=glm_pred)
+    evaluation_matrix.index = [
+        'Baseline', 'Linear Regression', 'LASSO LARS', 'General Linear Model']
+    evaluation_matrix.columns = ['Train RMSE', 'Validate RMSE']
+    return md('| Methodology' + evaluation_matrix.to_markdown()[1:]), llars
+
+
+def run_test(model: LinearRegressionType,
+             xtest: ModelDataType, ytest: ModelDataType) -> md:
     '''
     Runs best performing regression model on test data set.
     ## Parameters
@@ -155,7 +183,6 @@ def run_test(model:LinearRegressionType,xtest:ModelDataType,ytest:ModelDataType)
         of predictions on test data set.
     '''
     ypred = model.predict(xtest)
-    ev.plot_residuals(ytest.tax_value,ypred)
-    return md('### Mean Squared Error: ' + \
-        str(np.round(np.sqrt(mean_squared_error(ytest,ypred)),2)))
-        
+    ev.plot_residuals(ytest.tax_value, ypred)
+    return md('### Mean Squared Error: ' +
+              str(np.round(np.sqrt(mean_squared_error(ytest, ypred)), 2)))
